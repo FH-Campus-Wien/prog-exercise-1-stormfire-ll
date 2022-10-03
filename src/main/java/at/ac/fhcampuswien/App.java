13bbd5cb703ba5a9bdd8cc403cb9b48f3cbb75b9
stormfire-ll <tinglee867@gmail.com>
2022-10-04 01:14:14 +0200

    Task 9

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,33 +6,96 @@ public class App {
 
     //todo Task 1
     public void sayHelloWorld(){
-        // input your solution here
         System.out.println("Hello World!");
     }
 
     //todo Task 2
     public void helloRobot(){
-        // input your solution here
+                System.out.println("0123456789012345678901" + System.lineSeparator() +
+                "         __" + System.lineSeparator() +
+                " _(\\    |@@|" + System.lineSeparator() +
+                "(__/\\__ \\--/ __" + System.lineSeparator() +
+                "   \\___|----|  |   __" + System.lineSeparator() +
+                "       \\ }{ /\\ )_ / _\\" + System.lineSeparator() +
+                "       /\\__/\\ \\__O (__" + System.lineSeparator() +
+                "      (--/\\--)    \\__/" + System.lineSeparator() +
+                "      _)(  )(_" + System.lineSeparator() +
+                "     `---''---`");
     }
 
     //todo Task 3
     public void sumOfLiterals(){
-        // input your solution here
+
+        char z = 'Z';
+        String oxface = "0xface";
+        int octal = 012;
+        String volume = "80L";
+        float height = 44e-1f; //?
+        float outcome = 5.5f;
+        double length = 8.88e1;
+        float num = 99.9f;
+
+        int sum;
+        int x = 1;
+        int y = 2;
+        int a = 3;
+        int c = 4;
+        int d = 5;
+        sum = x + y + a + c + d;
+        System.out.println(sum);
     }
 
     //todo Task 4
     public void addTwoNumbers(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int num1;
+        int num2;
+        int sum;
+
+        num1 = scanner.nextInt();
+        num2 = scanner.nextInt();
+        sum = num1 + num2;
+        System.out.println(sum);
     }
 
     //todo Task 5
     public void swapTwoNumbers(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+
+        int x;
+        int y;
+
+        System.out.println("Before Swap:");
+        x = scanner.nextInt();
+        System.out.println("x: " + x);
+        y = scanner.nextInt();
+        System.out.println("y: " + y);
+        System.out.println("After Swap:");
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
     }
 
     //todo Task 6
     public void compareTwoNumbers(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int n1;
+        int n2;
+
+        n1 = scanner.nextInt();
+        n2 = scanner.nextInt();
+
+        if (n1 != n2) {
+            if (n1 < n2) {
+                System.out.println("n1 < n2");
+            } else {
+                System.out.println("n1 > n2");
+            }
+        } else {
+            System.out.println("n1 == n2");
+        }
     }
 
     //todo Task 7
@@ -47,12 +110,18 @@ public class App {
 
     //todo Task 9
     public void leapyear(){
-        // input your solution here
+        Scanner scanner = new Scanner(System.in);
+        int year;
+
+        year = scanner.nextInt();
+        System.out.println("Year: " + year);
+
+        if (year % 4 != 0)
     }
 
     //todo Task 10
     public void transposedNumbers(){
-        // input your solution here
+
     }
 
 
